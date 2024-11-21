c8e2a9fc51555868787b9415849d00d082cf6535
LIMA ALBUQUERQUE DE TORRES LUCAS <lucas,limat@ufrpe.br>
2024-11-21 10:22:43 -0300

    Adição do sistema de vínculos

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Gerar Declaração</title>
+    <title>Menu de Opções</title>
 </head>
 <body>
-    <h1>Gerar Declaração</h1>
-    <form action="declaracao_novo.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome" required><br><br>
-        <label for="sobrenome">Sobrenome:</label>
-        <input type="text" id="sobrenome" name="sobrenome" required><br><br>
-        <button type="submit">Gerar Declaração</button>
-    </form>
+    <h1>Escolha uma opção</h1>
+    
+    <!-- Link para Gerar Declaração -->
+    <p><a href="gerar_declaracao.php">Gerar Declaração</a></p>
+
+    <!-- Link para Buscar por CPF -->
+    <p><a href="vinculo.php">Cadastrar Vínculo</a></p>
 </body>
 </html>
