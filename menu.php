c08a56fa8c9b0b328b0f7d3dffe053d0d770749e
LIMA ALBUQUERQUE DE TORRES LUCAS <lucas,limat@ufrpe.br>
2024-11-29 11:38:53 -0300

    menu.php

---
+++ menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu de Opções</title>
+</head>
+<body>
+    <h1>Escolha uma opção</h1>
+
+    <!-- Link para Gerar Declaração -->
+    <p><a href="gerar_declaracao.php">Gerar Declaração</a></p>
+
+    <!-- Link para cadastar Vínculo -->
+    <p><a href="vinculo.php">Cadastrar Vínculo</a></p>
+
+    <!-- Link para buscar Vínculo por CPF -->
+    <p><a href="consulta_cpf.php">Buscar Vínculo</a></p>
+
+    <!-- Link para buscar Vínculo pelo Hash Code -->
+    <p><a href="consulta_hash.php">Buscar Hash Code</a></p>
+
+</body>
+</html>
